78fc57fe995f246e91c496c4320a919ce5792d8a
vnln <bananiqus@gmail.com>
2022-04-27 01:28:22 +0400

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,27 +1,31 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY = 15; //decay rate
+const HALF_LIFE_PERIOD = 5730; // years
 
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sample) {
+  const decayConstant = 0.693 / HALF_LIFE_PERIOD;
+  const regex = /^\d+\.*\d*$/;
+  return typeof sample !== "string" || !regex.test(sample) || sample <= 0 || sample >= MODERN_ACTIVITY
+    ? false
+    : Math.ceil(Math.log(MODERN_ACTIVITY / sample) / decayConstant);
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
