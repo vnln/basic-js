3ca78faad6b4448cadd0257f69319a468d97ccb6
vnln <bananiqus@gmail.com>
2022-04-27 20:56:42 +0400

    feat: common char, delete digits

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(num) {
+  return Math.max(...[...(num + "")].map((digit, index) => String(num).substring(0, index) + String(num).substring(index + 1)));
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
